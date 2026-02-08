4578c031059335e33a1440bfe7408b130ab100c8
cmniveditha99-png <cm.niveditha99@gmail.com>
2026-02-08 10:27:30 +0530

    Delete functions directory

--- functions/[[path]].js
+++
@@ -1,5 +0,0 @@
-export async function onRequest(context) {
-  // This forces ALL routes to go through Pages Functions,
-  // so your _middleware.js runs for /mock-01, /mock-04, etc.
-  return context.next();
-}
