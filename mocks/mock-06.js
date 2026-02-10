0632b44837758ded72122c49a328e22ec101918d
cmniveditha99-png <cm.niveditha99@gmail.com>
2026-02-10 15:14:34 +0530

    Add files via upload

---
+++ mocks/mock-06.js
@@ -0,0 +1,23 @@
+window.CCL_MOCK = {
+  segments: [
+    
+    { id: "seg01", title: "Segment 1", audioUrl: "https://pub-15435c2c49624fce863d237aeb26f8b7.r2.dev/Community%20Hall%20Issues/Default%20Project01.mp3" },
+    { id: "seg02", title: "Segment 2", audioUrl: "https://pub-15435c2c49624fce863d237aeb26f8b7.r2.dev/Community%20Hall%20Issues/Default%20Project02.mp3" },
+    { id: "seg03", title: "Segment 3", audioUrl: "https://pub-15435c2c49624fce863d237aeb26f8b7.r2.dev/Community%20Hall%20Issues/Default%20Project03.mp3" },
+    { id: "seg04", title: "Segment 4", audioUrl: "https://pub-15435c2c49624fce863d237aeb26f8b7.r2.dev/Community%20Hall%20Issues/Default%20Project04.mp3" },
+    { id: "seg05", title: "Segment 5", audioUrl: "https://pub-15435c2c49624fce863d237aeb26f8b7.r2.dev/Community%20Hall%20Issues/Default%20Project05.mp3" },
+    { id: "seg06", title: "Segment 6", audioUrl: "https://pub-15435c2c49624fce863d237aeb26f8b7.r2.dev/Community%20Hall%20Issues/Default%20Project06.mp3" },
+    { id: "seg07", title: "Segment 7", audioUrl: "https://pub-15435c2c49624fce863d237aeb26f8b7.r2.dev/Community%20Hall%20Issues/Default%20Project07.mp3" },
+    { id: "seg08", title: "Segment 8", audioUrl: "https://pub-15435c2c49624fce863d237aeb26f8b7.r2.dev/Community%20Hall%20Issues/Default%20Project08.mp3" },
+    { id: "seg09", title: "Segment 9", audioUrl: "https://pub-15435c2c49624fce863d237aeb26f8b7.r2.dev/Community%20Hall%20Issues/Default%20Project09.mp3" },
+    { id: "seg10", title: "Segment 10", audioUrl: "https://pub-15435c2c49624fce863d237aeb26f8b7.r2.dev/Community%20Hall%20Issues/Default%20Project10.mp3" },
+    { id: "seg11", title: "Segment 11", audioUrl: "https://pub-15435c2c49624fce863d237aeb26f8b7.r2.dev/Community%20Hall%20Issues/Default%20Project11.mp3" },
+    { id: "seg12", title: "Segment 12", audioUrl: "https://pub-15435c2c49624fce863d237aeb26f8b7.r2.dev/Community%20Hall%20Issues/Default%20Project12.mp3" },
+    { id: "seg13", title: "Segment 13", audioUrl: "https://pub-15435c2c49624fce863d237aeb26f8b7.r2.dev/Community%20Hall%20Issues/Default%20Project13.mp3" },
+    { id: "seg14", title: "Segment 14", audioUrl: "https://pub-15435c2c49624fce863d237aeb26f8b7.r2.dev/Community%20Hall%20Issues/Default%20Project14.mp3" }
+
+      ]
+};
+
+
+
