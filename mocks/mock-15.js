94d51b0df1c91708f6cf593f5f2a1433db590190
cmniveditha99-png <cm.niveditha99@gmail.com>
2026-08-04 12:27:29 +0530

    Add files via upload

---
+++ mocks/mock-15.js
@@ -0,0 +1,20 @@
+window.CCL_MOCK = {
+  segments: [
+    
+{ id: "seg01", title: "Segment 1", audioUrl: "https://pub-15435c2c49624fce863d237aeb26f8b7.r2.dev/Creative%20Writing%20Course/01_Eng_01_with_beep.wav" },
+{ id: "seg02", title: "Segment 2", audioUrl: "https://pub-15435c2c49624fce863d237aeb26f8b7.r2.dev/Creative%20Writing%20Course/02_Mal_01_with_beep.wav" },
+{ id: "seg03", title: "Segment 3", audioUrl: "https://pub-15435c2c49624fce863d237aeb26f8b7.r2.dev/Creative%20Writing%20Course/03_Eng_02_with_beep.wav" },
+{ id: "seg04", title: "Segment 4", audioUrl: "https://pub-15435c2c49624fce863d237aeb26f8b7.r2.dev/Creative%20Writing%20Course/04_Mal_02_with_beep.wav" },
+{ id: "seg05", title: "Segment 5", audioUrl: "https://pub-15435c2c49624fce863d237aeb26f8b7.r2.dev/Creative%20Writing%20Course/05_Eng_03_with_beep.wav" },
+{ id: "seg06", title: "Segment 6", audioUrl: "https://pub-15435c2c49624fce863d237aeb26f8b7.r2.dev/Creative%20Writing%20Course/06_Mal_03_with_beep.wav" },
+{ id: "seg07", title: "Segment 7", audioUrl: "https://pub-15435c2c49624fce863d237aeb26f8b7.r2.dev/Creative%20Writing%20Course/07_Eng_04_with_beep.wav" },
+{ id: "seg08", title: "Segment 8", audioUrl: "https://pub-15435c2c49624fce863d237aeb26f8b7.r2.dev/Creative%20Writing%20Course/08_Mal_04_with_beep.wav" },
+{ id: "seg09", title: "Segment 9", audioUrl: "https://pub-15435c2c49624fce863d237aeb26f8b7.r2.dev/Creative%20Writing%20Course/09_Eng_05_with_beep.wav" },
+{ id: "seg10", title: "Segment 10", audioUrl: "https://pub-15435c2c49624fce863d237aeb26f8b7.r2.dev/Creative%20Writing%20Course/10_Mal_05_with_beep.wav" },
+{ id: "seg11", title: "Segment 11", audioUrl: "https://pub-15435c2c49624fce863d237aeb26f8b7.r2.dev/Creative%20Writing%20Course/11_Eng_06_with_beep.wav" },
+{ id: "seg12", title: "Segment 12", audioUrl: "https://pub-15435c2c49624fce863d237aeb26f8b7.r2.dev/Creative%20Writing%20Course/12_Mal_06_with_beep.wav" },
+{ id: "seg13", title: "Segment 13", audioUrl: "https://pub-15435c2c49624fce863d237aeb26f8b7.r2.dev/Creative%20Writing%20Course/13_Eng_07_with_beep.wav" },
+{ id: "seg14", title: "Segment 14", audioUrl: "https://pub-15435c2c49624fce863d237aeb26f8b7.r2.dev/Creative%20Writing%20Course/14_Mal_07_with_beep.wav" }
+
+      ]
+};
